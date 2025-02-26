914307b62320b63c0c7dca540a6db14fcc9c9777
snowm <snowm@192.168.2.105>
2025-02-26 13:54:26 +0900

    second

--- 3_2_java_advance/src/main/java/ServletPractice04.java
+++ 3_2_java_advance/src/main/java/ServletPractice04.java
@@ -3,7 +3,6 @@
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ServletPractice04
  */
-@WebServlet("/aaa")
+
 public class ServletPractice04 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
