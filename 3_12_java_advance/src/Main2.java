22eb6116e047f3797cf53dd107eb8fc630241bd1
snowm <snowm@192.168.2.105>
2025-04-19 20:36:39 +0900

    second

--- 3_12_java_advance/src/Main2.java
+++ 3_12_java_advance/src/Main2.java
@@ -1,15 +1,15 @@
-
 // 実行クラス
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main2 {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(10, 5, 2, 20, 12, 15);
-        int sum = 0;
-        for (int number : numbers) {
-            sum += number;
-        }
-        System.out.println(sum);
+        AtomicInteger sum = new AtomicInteger(0);
+
+        numbers.forEach(number -> sum.addAndGet(number));
+
+        System.out.println(sum.get());
     }
 }
