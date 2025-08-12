819569b413f8726ff71f6f7fb9db078c3df6ef1e
snowm <snowm@192.168.2.105>
2025-08-12 13:48:21 +0900

    質問あります

--- 5_4_framework/src/main/java/com/example/Application.java
+++ 5_4_framework/src/main/java/com/example/Application.java
@@ -1,9 +1,7 @@
-package com.example.four;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.example.Application;
 
 @SpringBootApplication
 public class Application {
